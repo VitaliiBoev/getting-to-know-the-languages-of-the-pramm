7f9b08d0e42c36525c2694e41c724e8b9d675218
Vitaliy B <boev182@gmail.com>
2023-12-14 21:05:46 +0500

    третья задача

--- Program.cs
+++ Program.cs
@@ -44,3 +44,27 @@ namespace EventNumbersInArray
         }
     }
 }
+//--------------------------------------------------------------------
+
+/*Задача 3 
+Задайте массив из вещественных чисел с ненулевой дробной частью.
+Найдите разницу между максимальным и минимальным элементов массива.*/
+    
+class Program
+
+{
+        static void Main()
+    {
+        Random rnd = new Random();
+        double[] array = Enumerable.Range(0, 10).Select(x => rnd.NextDouble() *10-5).ToArray();
+
+        double minValue = array.Min();
+        double maxValue = array.Max();
+
+        double difference = maxValue - minValue;
+
+        Console.WriteLine("Минимальный эллемент: " + minValue);
+        Console.WriteLine("Максимальный эллемент: " + maxValue);
+        Console.WriteLine("Разница между ними: " + difference);
+    }
+}
